d2931ce160b0dfdc0c8ec92aa3cc270fd32c9048
xiaopch <xiaopch@163.com>
2025-02-27 12:57:29 +0800

    Today.Do it.

---
+++ git_clone.ssh.github.blog.cmd
@@ -0,0 +1,6 @@
+#!/bin/bash
+git status
+cmd /k   git clone git@github.com:xiaopch/blog.git
+
+
+sleep 30
---
+++ git_push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git status
+git add -A
+git status
+git commit -m "Today.Do it."
+git push
+sleep 3
